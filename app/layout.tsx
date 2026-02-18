8872efd3fc37d1260f7be08206658ce96e0c1be7
Vaishnav <vaishnavprabhakaran31@gmail.com>
2026-02-18 15:10:55 +0530

    chore: add google site verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     canonical: '/',
   },
   verification: {
-    google: 'google-site-verification=YOUR_CODE_HERE', // User needs to update this
+    google: 'google-site-verification=fXcPiXBPbANtEb_BMUGKo3wbQmPi7rK6E52JCV2MpTU', // User needs to update this
   },
 };
 
